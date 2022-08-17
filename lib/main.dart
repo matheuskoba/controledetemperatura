0c5a80c762d2d33d509e22a2fe57fbd703b9c5fb
Matheus Kobayashi <koba@pop-os.localdomain>
2022-08-17 14:27:03 -0300

    create a increment and decrement counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,104 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp( App() );
+void main() => runApp(const App());
 
-class App extends StatelessWidget { App ({ super.key});
-
-  var temperatura = 0;
-
-  aumentartemperatura(){
-    return temperatura + 1;
-  }
-  diminuirtemperatura(){
-    return temperatura - 1;
-  }
-  alterartemperatura(){
-    print('Alterou a temperatura');
-  }
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-              title: Center(
-                key: key,
-                child: const Text("Seja bem vindo")
-              )
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                const Text(
-                 'Controle de temperatura',
-                  style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold
-                  ),
-                ),
-                Container(
+      title: 'Controle de temperatura',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Controle de temperatura')
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  get key => null;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Center(
+              key: key,
+              child: Text(widget.title)
+          )
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                   key: key,
-                  margin: const EdgeInsetsDirectional.only(top: 20),
-                  child: const Icon(Icons.ac_unit, size: 40, color: Colors.blue)
-                ),
-                Container(
+                  margin: const EdgeInsetsDirectional.only(bottom: 40),
+                  child: const Icon(Icons.ac_unit, size: 80, color: Colors.blue)
+              ),
+              const Text(
+                'Temperatura',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                  )
+              ),
+              Container(
                   key: key,
                   margin: const EdgeInsetsDirectional.only(top: 30),
-                  child: Text(temperatura.toString(), style: const TextStyle(fontSize: 40, color: Colors.indigo, fontWeight: FontWeight.bold))
-                ),
-                Row(
-                  children: [
-                    Expanded(
+                  child: Text('$_counter', style: const TextStyle(fontSize: 40, color: Colors.indigo, fontWeight: FontWeight.bold))
+              ),
+              Row(
+                children: [
+                  Expanded(
                       child: Container(
-                        key: key,
-                        margin: const EdgeInsetsDirectional.only(top: 20),
-                        child: TextButton(onPressed: diminuirtemperatura, child: const Text('-', style: TextStyle(fontSize: 50, color: Colors.red)))
+                          key: key,
+                          margin: const EdgeInsetsDirectional.only(top: 20),
+                          child: TextButton(onPressed: _decrementCounter, child: const Text('-', style: TextStyle(fontSize: 50, color: Colors.red)))
                       )
-                    ),
-                    Expanded(
+                  ),
+                  Expanded(
                       child: Container(
-                        key: key,
-                        margin: const EdgeInsetsDirectional.only(top: 20),
-                        child: TextButton(onPressed: aumentartemperatura, child: const Text('+', style: TextStyle(fontSize: 50, color: Colors.blue),))
+                          key: key,
+                          margin: const EdgeInsetsDirectional.only(top: 20),
+                          child: TextButton(onPressed: _incrementCounter, child: const Text('+', style: TextStyle(fontSize: 50, color: Colors.blue),))
                       )
-                    )
-                  ],
-                ),
-                TextButton(onPressed: alterartemperatura, child: const Text('Alterar temperatura'))
-              ],
-            )
+                  )
+                ],
+              ),
+              Container(
+                margin: const EdgeInsetsDirectional.only(top: 40),
+                child: TextButton(onPressed: (){print('Alterou a temperatura');}, child: const Text('Alterar')),
+              )
+            ],
           )
-      )
-    );
+        )
+      );
   }
 
 }
